3c440c2e070221cd3dbb6bdc0308cc80f28b5676
Jonnie Möller <scarabeo85@msn.com>
2024-04-17 22:48:10 +0200

    test_14

--- server/utils/fetchUsers.js
+++ server/utils/fetchUsers.js
@@ -1,18 +1,18 @@
-const fs = require("fs").promises;
 const path = require("path");
+const fs = require("fs").promises;
 
 const fetchUsers = async () => {
-  const filePath = path.join(__dirname, "./data/users.json");
-  console.log(`Attempting to read users from ${filePath}`);
   
+  const filePath = path.join(__dirname, '..', 'data', 'users.json');
+
+  console.log(`Attempting to read users from ${filePath}`);
+
   try {
-    const data = await fs.readFile(filePath);
-    const users = JSON.parse(data);
-    console.log(`Found ${users.length} users`);
-    return users;
+    const data = await fs.readFile(filePath, "utf8");
+    return JSON.parse(data);
   } catch (error) {
-    console.error("Failed to fetch users:", error);
-    throw error; // or handle the error as needed
+    console.error(`Failed to fetch users from ${filePath}:`, error);
+    throw error; d
   }
 };
 
